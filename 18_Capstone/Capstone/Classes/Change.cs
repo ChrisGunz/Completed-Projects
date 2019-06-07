1f9627065f5bac0d23a68354680766da5d39725d
Chris Gunzenhauser <cngunzenhauser@gmail.com>
2019-06-07 10:45:59 -0400

    change and money class maybe trial code

--- 18_Capstone/Capstone/Classes/Change.cs
+++ 18_Capstone/Capstone/Classes/Change.cs
@@ -4,11 +4,45 @@ using System.Text;
 
 namespace Capstone.Classes
 {
-    public class Change
+    public class Change 
     {
-        public decimal TotalValue()
-        {
-            return 0.0M; // might be derived ?
+        
+        
+        //public int TenDollarBills { get; }
+        //public int FiveDollarBills { get; }
+        //public int DollarBills { get; }
+        //public int Quarters { get; }
+        //public int Dimes { get; }
+        //public int Nickels { get; }
+        //public int Pennies { get; }
+
+        //public Change(decimal balanceToReturn)
+        //{
+       
+       
+
+        //    TenDollarBills = (int)(balanceToReturn / 10);
+        //    balanceToReturn %= 10;
+
+        //    FiveDollarBills = (int)(balanceToReturn / 5);
+        //    balanceToReturn %= 5;
+
+        //    DollarBills = (int)(balanceToReturn / 1);
+        //    balanceToReturn %= 1;
+
+        //    Quarters = (int)(balanceToReturn / .25m);
+        //    balanceToReturn %= .25m;
+
+        //    Dimes = (int)(balanceToReturn / .10m);
+        //    balanceToReturn %= .10m;
+
+        //    Nickels = (int)(balanceToReturn / .05m);
+        //    balanceToReturn %= .05m;
+
+        //    Pennies = (int)(balanceToReturn / .01m);
         }
+
+
+
     }
 }
--- 18_Capstone/Capstone/Classes/VendingMachine.cs
+++ 18_Capstone/Capstone/Classes/VendingMachine.cs
@@ -4,10 +4,23 @@ using System.Text;
 
 namespace Capstone.Classes
 {
-    public class VendingMachine
+    public class VendingMachine 
     {
         public Dictionary <string,List<Item>> Stock { get; private set; }
+      
+        // TRAIL CODE ********************************
+        //public decimal Balance { get; }
+       
 
+        //public VendingMachine(decimal currentBalance)
+        //{
+        //    currentBalance = 0;
+        //    this.Balance = currentBalance;
+        //}
+
+        /// trial code
+        
+       
         public void Load(Dictionary<string, List<Item>> items)
         {
             this.Stock = items;
